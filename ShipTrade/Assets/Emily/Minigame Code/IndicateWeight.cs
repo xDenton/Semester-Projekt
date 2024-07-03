a4f5c2f1adf4a63bf1f4b66e20142bdc9cffb3fa
Paul <pagoetz04@gmail.com>
2024-07-03 19:59:05 +0200

    Minigame gefixt

--- ShipTrade/Assets/Emily/Minigame Code/IndicateWeight.cs
+++ ShipTrade/Assets/Emily/Minigame Code/IndicateWeight.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class IndicateWeight : MonoBehaviour
 {
     private float movementSpeed = 50f;
-    public float vecX = -810f;
+    public float vecX = -26.71f;
     public CalculateCargoWeight CalculateCargoWeight;
     public RectTransform RectTransform;
 
+    //Werte vorher (-810/-455/0)
     void Start()
     {
-        RectTransform.SetLocalPositionAndRotation(new Vector3(-810, -455, 0), new Quaternion(0, 0, 0, 0));
+        RectTransform.SetLocalPositionAndRotation(new Vector3(-26.71f, -16.3f, 0), new Quaternion(0, 0, 0, 0));
     }
 
     public void AddIndicateWeight(int weight)
@@ -23,9 +24,11 @@ public class IndicateWeight : MonoBehaviour
             RectTransform.transform.position = RectTransform.transform.position + new Vector3(weight * movementSpeed * Time.deltaTime, 0, 0);
         }
         else
+        //x 810/ -455/0
         {
-            RectTransform.SetLocalPositionAndRotation(new Vector3(810, -455, 0), new Quaternion(0, 0, 0, 0));
-            vecX = 810;
+            RectTransform.SetLocalPositionAndRotation(new Vector3(-26.71f, -16.3f, 0), new Quaternion(0, 0, 0, 0));
+            //vorher 810
+            vecX = -26.71f;
         }
     }
 
@@ -33,14 +36,15 @@ public class IndicateWeight : MonoBehaviour
     {
         vecX -= weight * movementSpeed * Time.deltaTime;
 
+        //Werte vorher (-810/-455/0)
         if (vecX > -809)
         {
             RectTransform.transform.position = RectTransform.transform.position - new Vector3(weight * movementSpeed * Time.deltaTime, 0, 0);
         }
         else
         {
-            RectTransform.SetLocalPositionAndRotation(new Vector3(-810, -455, 0), new Quaternion(0, 0, 0, 0));
-            vecX = -810;
+            RectTransform.SetLocalPositionAndRotation(new Vector3(-26.71f, -16.3f, 0), new Quaternion(0, 0, 0, 0));
+            vecX = -26.71f;
         }
     }
 }
