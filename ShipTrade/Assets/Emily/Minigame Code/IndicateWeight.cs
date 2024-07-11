068800ad45e77fae045598c4f0c223ce25bbb4a4
em011 <emilypolitt1@gmail.com>
2024-07-11 16:44:31 +0200

    Anpassung Werte für Gewichtsanzeige

--- ShipTrade/Assets/Emily/Minigame Code/IndicateWeight.cs
+++ ShipTrade/Assets/Emily/Minigame Code/IndicateWeight.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class IndicateWeight : MonoBehaviour
 {
     private float movementSpeed = 50f;
-    public float vecX = -26.71f;
+    private float vecX = -715.5f;
     public CalculateCargoWeight CalculateCargoWeight;
     public RectTransform RectTransform;
 
-    //Werte vorher (-810/-455/0)
     void Start()
     {
-        RectTransform.SetLocalPositionAndRotation(new Vector3(-26.71f, -16.3f, 0), new Quaternion(0, 0, 0, 0));
+        RectTransform.SetLocalPositionAndRotation(new Vector3(-715.5f, -405f, 0), new Quaternion(0, 0, 0, 0));
+        //Werte vorher (-810/-455/0)
     }
 
     public void AddIndicateWeight(int weight)
     {
         vecX += weight * movementSpeed * Time.deltaTime;
 
-        if (vecX < 809)
+        if (vecX < 715f)
         {
             RectTransform.transform.position = RectTransform.transform.position + new Vector3(weight * movementSpeed * Time.deltaTime, 0, 0);
         }
         else
-        //x 810/ -455/0
         {
-            RectTransform.SetLocalPositionAndRotation(new Vector3(-26.71f, -16.3f, 0), new Quaternion(0, 0, 0, 0));
+            RectTransform.SetLocalPositionAndRotation(new Vector3(-715.5f, -405f, 0), new Quaternion(0, 0, 0, 0));
+            //x 810/ -455/0
+            vecX = 715.5f;
             //vorher 810
-            vecX = -26.71f;
         }
     }
 
@@ -36,15 +36,15 @@ public class IndicateWeight : MonoBehaviour
     {
         vecX -= weight * movementSpeed * Time.deltaTime;
 
-        //Werte vorher (-810/-455/0)
-        if (vecX > -809)
+        if (vecX > -715f)
         {
             RectTransform.transform.position = RectTransform.transform.position - new Vector3(weight * movementSpeed * Time.deltaTime, 0, 0);
         }
         else
         {
-            RectTransform.SetLocalPositionAndRotation(new Vector3(-26.71f, -16.3f, 0), new Quaternion(0, 0, 0, 0));
-            vecX = -26.71f;
+            RectTransform.SetLocalPositionAndRotation(new Vector3(-715.5f, -405f, 0), new Quaternion(0, 0, 0, 0));
+            //Werte vorher (-810/-455/0)
+            vecX = -715.5f;
         }
     }
 }
